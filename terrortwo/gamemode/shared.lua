11a11e5efec9c8d90d6da3d674d0c6a2ddbaeec2
George Petrou <gspetrou98@gmail.com>
2016-08-25 23:33:40 -0400

    Fixed bug and enhanced module loading
    Fix bug with reading file prefixes, added hook for post Modules loaded.

--- terrortwo/gamemode/shared.lua
+++ terrortwo/gamemode/shared.lua
@@ -4,10 +4,11 @@ GM.Email = "N/A"
 GM.Website = "N/A"
 
 DEFINE_BASECLASS("gamemode_base")
+local debugmode = CreateConVar("ttt_debug_prints", "1", nil, "Enables debug prints.")
 
 TTT = TTT or {}
 
-local function SetupFile(path, prefix, modulename)
+local function SetupFile(path, prefix)
 	if prefix == "sv_" then
 		if SERVER then
 			include(path)
@@ -33,11 +34,15 @@ GM.Modules = {}
 function GM:LoadModules()
 	local _, addon_module_folders = file.Find("modules/*", "LUA")
 	for _, v in ipairs(addon_module_folders) do
-		GM.Modules[v] = true
+		if not GM.Modules[v] then
+			GM.Modules[v] = true
 
-		local addon_module_files = file.Find("modules/"..v.."/*.lua", "LUA")
-		for _, j in ipairs(addon_module_files) do
-			SetupFile("modules/"..v.."/"..j, j:sub(1, 4))
+			local addon_module_files = file.Find("modules/"..v.."/*.lua", "LUA")
+			for _, j in ipairs(addon_module_files) do
+				SetupFile("modules/"..v.."/"..j, j:sub(1, 3))
+			end
+		elseif debugmode:GetBool() then
+			DebugPrint("Module with the name '"..v.."' already has been mounted.")
 		end
 	end
 
@@ -49,10 +54,14 @@ function GM:LoadModules()
 
 			local gm_module_files = file.Find(root..v.."/*.lua", "LUA")
 			for _, j in ipairs(gm_module_files) do
-				SetupFile(root..v.."/"..j, j:sub(1, 4))
+				SetupFile(root..v.."/"..j, j:sub(1, 3))
 			end
+		elseif debugmode:GetBool() then
+			DebugPrint("Module with the name '"..v.."' already has been mounted.")
 		end
 	end
+
+	hook.Call("TTT_PostModulesLoaded", GM)
 end
 
 GM:LoadModules()
