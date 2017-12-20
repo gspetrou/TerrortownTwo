962992ced776c7da7a3bf370aba5d55ee609cc7f
gspetrou <gspetrou98@gmail.com>
2017-12-20 14:35:53 -0500

    Expanded on dev library
    Just some stuff to mess with bots better.

--- terrortwo/gamemode/library/_dev/sh_dev.lua
+++ terrortwo/gamemode/library/_dev/sh_dev.lua
@@ -10,6 +10,17 @@ if SERVER then
 	concommand.Add("lr", function(_, _, _, argstr)
 		RunString(argstr)	-- Im lazy
 	end)
+
+	concommand.Add("addbots", function(_, _, args)
+		for i = 1, args[1] do
+			RunConsoleCommand("bot")
+		end
+	end)
+	concommand.Add("kickbots", function()
+		for i, v in ipairs(player.GetBots()) do
+			v:Kick("You're a bot")
+		end
+	end)
 else
 	concommand.Add("lrc", function(_, _, _, argstr)
 		RunString(argstr)
@@ -18,4 +29,13 @@ end
 
 function Stalker()
 	return player.GetBySteamID("STEAM_0:1:18093014")
+end
+
+function GetBot(num)
+	for i, v in ipairs(player.GetBots()) do
+		local endNum = tonumber(string.sub(v:Nick(), 4))
+		if endNum == num then
+			return v
+		end
+	end
 end
